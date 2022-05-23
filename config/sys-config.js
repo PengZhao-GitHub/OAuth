07d1056ae293ae5333a3de2c744c2b8fe08863cc
PengZhao-GitHub <53984065+PengZhao-GitHub@users.noreply.github.com>
2022-05-23 14:18:19 +0900

    Update sys-config.js

--- config/sys-config.js
+++ config/sys-config.js
@@ -13,7 +13,8 @@ if(process.argv[2]=='develop'){
 module.exports = {
     log_in_out_callBackUrl: {
         DevMode: '/profile/',
-        ServiceMode: 'http://insurance-customer-portal.s3-website-ap-northeast-1.amazonaws.com/profile/'
+        //ServiceMode: 'http://insurance-customer-portal.s3-website-ap-northeast-1.amazonaws.com/profile/'
+        ServiceMode: 'http://3.91.100.67:4200//profile/'
     },
     DevMode: devFlag  //control the call back URL
 };
