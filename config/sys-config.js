d8122c69290b84150d6d1cf65b10e727ef51c2ed
PengZhao-GitHub <53984065+PengZhao-GitHub@users.noreply.github.com>
2022-06-03 13:46:03 +0900

    Update sys-config.js
    Update the call back IP with URL

--- config/sys-config.js
+++ config/sys-config.js
@@ -14,7 +14,7 @@ module.exports = {
     log_in_out_callBackUrl: {
         DevMode: '/profile/',
         //ServiceMode: 'http://insurance-customer-portal.s3-website-ap-northeast-1.amazonaws.com/profile/'
-        ServiceMode: 'http://18.212.214.94:4200/profile/'
+        ServiceMode: 'http://www.insurancecustomerportal.com:4200/profile/'
     },
     DevMode: devFlag  //control the call back URL
 };
